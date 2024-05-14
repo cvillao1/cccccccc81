bb006bc3071f506b128074ebd3fb6afb97dd79c0
cvillao1 <cvillaom2005@gmail.com>
2024-05-14 18:14:49 -0500

    yaporfa3

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -82,14 +82,10 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mysite',
-        'USER': 'mysite_yv0m_user',
-        'PASSWORD': 'nOmCeKc9ljrcT4UDLfrNMj8p8NkFxs1N',
-        'HOST': 'dpg-cp1ukg6n7f5s73f891sg-a',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost/postgres',
+        conn_max_age=600
+    )
 }
 
 
@@ -128,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# This production code might break development mode, so we check whether we're in DEBUG mode
+
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
