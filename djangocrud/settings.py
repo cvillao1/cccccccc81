4e3f120776329991d46cc7a779a2018221ad9940
cvillao1 <cvillaom2005@gmail.com>
2024-05-14 18:08:12 -0500

    yaporfa2

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mysite',
+        'USER': 'mysite_yv0m_user',
+        'PASSWORD': 'nOmCeKc9ljrcT4UDLfrNMj8p8NkFxs1N',
+        'HOST': 'dpg-cp1ukg6n7f5s73f891sg-a',
+        'PORT': '5432',
+    }
 }
 
 
